00e91bf822984b1e6a4015d86a6385980f500537
Arpit Jain <arpit.jain2811@gmail.com>
2015-04-02 00:25:02 -0400

    Reindent

--- features/create_feat_mallet_crf_test.py
+++ features/create_feat_mallet_crf_test.py
@@ -1,15 +1,15 @@
 back_window_size = 1
 forward_window_size = 0
 boolean_features = True
-test_file = open("../data/test.txt","r")
-test_features_file = open("test_features_crf.txt","w")
+test_file = open("../data/test.txt", "r")
+test_features_file = open("test_features_crf.txt", "w")
 words = [];
 poss = [];
 tags = [];
 for line in test_file:
     p = line.strip().split(" ")
     if len(p) > 1:
-        word,pos,tag = line.rstrip().split(" ")
+        word, pos, tag = line.rstrip().split(" ")
         words.append(word)
         poss.append(pos)
         tags.append(tag)
@@ -27,31 +27,29 @@ next_word_features = []
 prev_shape_features = []
 next_shape_features = []
 
-
-
-for i in range(0,len(words)):
+for i in range(0, len(words)):
     feature = ""
     current_word = words[i]
     current_pos = poss[i]
 
     idx_back = back_window_size
-    for j in range(i-back_window_size,i):
+    for j in range(i - back_window_size, i):
         if j < 0:
-            feature = feature + default_word + " " + default_pos+" "
-            feature = feature + str(default_upper)+" "
-            feature = feature + str(default_upper)+" "
-            feature = feature + str(default_upper)+" "
-            feature = feature + str(default_upper)+" "
-            #feature = feature + default_word + " " + default_pos+" " +  default_pos+" "
+            feature = feature + default_word + " " + default_pos + " "
+            feature = feature + str(default_upper) + " "
+            feature = feature + str(default_upper) + " "
+            feature = feature + str(default_upper) + " "
+            feature = feature + str(default_upper) + " "
+            # feature = feature + default_word + " " + default_pos+" " +  default_pos+" "
         else:
-            feature = feature +  words[j] + " " + poss[j] + " "
-            feature = feature + str(int(str.isupper(words[j])))+" "
-            feature = feature + str(int(str.istitle(words[j])))+" "
-            feature = feature + str(int(str.islower(words[j])))+" "
-            feature = feature + str(int(str.isdigit(words[j])))+" "
-            #feature = feature +  words[j] + " " + poss[j] + " " + tags[j]+ " "
+            feature = feature + words[j] + " " + poss[j] + " "
+            feature = feature + str(int(str.isupper(words[j]))) + " "
+            feature = feature + str(int(str.istitle(words[j]))) + " "
+            feature = feature + str(int(str.islower(words[j]))) + " "
+            feature = feature + str(int(str.isdigit(words[j]))) + " "
+            # feature = feature +  words[j] + " " + poss[j] + " " + tags[j]+ " "
         idx_back = idx_back - 1
-    if boolean_features:        
+    if boolean_features:
         feature = feature + current_word + " " + current_pos + " "
         feature = feature + current_word.lower() + " "
         feature = feature + str(int(str.isalpha(current_word))) + " "
@@ -61,9 +59,9 @@ for i in range(0,len(words)):
         feature = feature + str(int(str.isupper(current_word))) + " "
 
     idx_forward = 1
-    for j in range(i+1,i+1+forward_window_size):
-        if j >= len(words) :
-            feature = feature + default_word + " " + default_pos+" "
+    for j in range(i + 1, i + 1 + forward_window_size):
+        if j >= len(words):
+            feature = feature + default_word + " " + default_pos + " "
         else:
             feature = feature + words[j] + " " + poss[j] + " "
         idx_forward = idx_forward + 1
--- features/create_feats.py
+++ features/create_feats.py
@@ -3,15 +3,15 @@ forward_window_size = 1
 boolean_features = True
 prev_tag = False
 
-train_file = open("../data/training.txt","r")
-train_features_file = open("train_features.txt","w")
+train_file = open("../data/training.txt", "r")
+train_features_file = open("train_features.txt", "w")
 words = [];
 poss = [];
 tags = [];
 for line in train_file:
     p = line.strip().split(" ")
     if len(p) > 1:
-        word,pos,tag = line.rstrip().split(" ")
+        word, pos, tag = line.rstrip().split(" ")
         words.append(word)
         poss.append(pos)
         tags.append(tag)
@@ -28,52 +28,55 @@ next_word_features = []
 prev_shape_features = []
 next_shape_features = []
 
-
-
-for i in range(0,len(words)):
+for i in range(0, len(words)):
     feature = ""
     current_word = words[i]
     current_pos = poss[i]
 
     idx_back = back_window_size
-    for j in range(i-back_window_size,i):
+    for j in range(i - back_window_size, i):
         if j < 0:
             if prev_tag:
-                feature = feature + "prev" + str(idx_back) + "word" + "=" + default_word + " " + "prev" + str(idx_back) + "pos" + "=" + default_pos+" " + "prev" + str(idx_back) + "tag" + "=" + default_pos+" "
-                feature = feature + "prev" + str(idx_back) + "isUpper" + "=" + str(default_upper)+" "
-                feature = feature + "prev" + str(idx_back) + "istitle" + "=" + str(default_upper)+" "
-                feature = feature + "prev" + str(idx_back) + "islower" + "=" + str(default_upper)+" "
-                feature = feature + "prev" + str(idx_back) + "isdigit" + "=" + str(default_upper)+" "
-                #feature = feature + "prev" + str(idx_back) + "isalpha" + "=" + str(default_upper)+" "
+                feature = feature + "prev" + str(idx_back) + "word" + "=" + default_word + " " + "prev" + str(
+                    idx_back) + "pos" + "=" + default_pos + " " + "prev" + str(
+                    idx_back) + "tag" + "=" + default_pos + " "
+                feature = feature + "prev" + str(idx_back) + "isUpper" + "=" + str(default_upper) + " "
+                feature = feature + "prev" + str(idx_back) + "istitle" + "=" + str(default_upper) + " "
+                feature = feature + "prev" + str(idx_back) + "islower" + "=" + str(default_upper) + " "
+                feature = feature + "prev" + str(idx_back) + "isdigit" + "=" + str(default_upper) + " "
+                # feature = feature + "prev" + str(idx_back) + "isalpha" + "=" + str(default_upper)+" "
             else:
-                feature = feature + "prev" + str(idx_back) + "word" + "=" + default_word + " " + "prev" + str(idx_back) + "pos" + "=" + default_pos+" "       
-                feature = feature + "prev" + str(idx_back) + "isUpper" + "=" + str(default_upper)+" "
-                feature = feature + "prev" + str(idx_back) + "istitle" + "=" + str(default_upper)+" "
-                feature = feature + "prev" + str(idx_back) + "islower" + "=" + str(default_upper)+" "
-                feature = feature + "prev" + str(idx_back) + "isdigit" + "=" + str(default_upper)+" "
-                #feature = feature + "prev" + str(idx_back) + "isalpha" + "=" + str(default_upper)+" "
+                feature = feature + "prev" + str(idx_back) + "word" + "=" + default_word + " " + "prev" + str(
+                    idx_back) + "pos" + "=" + default_pos + " "
+                feature = feature + "prev" + str(idx_back) + "isUpper" + "=" + str(default_upper) + " "
+                feature = feature + "prev" + str(idx_back) + "istitle" + "=" + str(default_upper) + " "
+                feature = feature + "prev" + str(idx_back) + "islower" + "=" + str(default_upper) + " "
+                feature = feature + "prev" + str(idx_back) + "isdigit" + "=" + str(default_upper) + " "
+                # feature = feature + "prev" + str(idx_back) + "isalpha" + "=" + str(default_upper)+" "
         else:
             if prev_tag:
-                feature = feature + "prev" + str(idx_back) + "word" + "=" + words[j] + " " + "prev" + str(idx_back) + "pos" + "=" + poss[j] + " " + "prev" + str(idx_back) + "tag" + "=" + tags[j]+ " "
-                feature = feature + "prev" + str(idx_back) + "isUpper" + "=" + str(int(str.isupper(words[j])))+" "
-                feature = feature + "prev" + str(idx_back) + "istitle" + "=" + str(int(str.istitle(words[j])))+" "
-                feature = feature + "prev" + str(idx_back) + "islower" + "=" + str(int(str.islower(words[j])))+" "
-                feature = feature + "prev" + str(idx_back) + "isdigit" + "=" + str(int(str.isdigit(words[j])))+" "
-                #feature = feature + "prev" + str(idx_back) + "isalpha" + "=" + str(int(str.isalpha(words[j])))+" "
+                feature = feature + "prev" + str(idx_back) + "word" + "=" + words[j] + " " + "prev" + str(
+                    idx_back) + "pos" + "=" + poss[j] + " " + "prev" + str(idx_back) + "tag" + "=" + tags[j] + " "
+                feature = feature + "prev" + str(idx_back) + "isUpper" + "=" + str(int(str.isupper(words[j]))) + " "
+                feature = feature + "prev" + str(idx_back) + "istitle" + "=" + str(int(str.istitle(words[j]))) + " "
+                feature = feature + "prev" + str(idx_back) + "islower" + "=" + str(int(str.islower(words[j]))) + " "
+                feature = feature + "prev" + str(idx_back) + "isdigit" + "=" + str(int(str.isdigit(words[j]))) + " "
+                # feature = feature + "prev" + str(idx_back) + "isalpha" + "=" + str(int(str.isalpha(words[j])))+" "
             else:
-                feature = feature + "prev" + str(idx_back) + "word" + "=" + words[j] + " " + "prev" + str(idx_back) + "pos" + "=" + poss[j] + " "
-                feature = feature + "prev" + str(idx_back) + "isUpper" + "=" + str(int(str.isupper(words[j])))+" "
-                feature = feature + "prev" + str(idx_back) + "istitle" + "=" + str(int(str.istitle(words[j])))+" "
-                feature = feature + "prev" + str(idx_back) + "islower" + "=" + str(int(str.islower(words[j])))+" "
-                feature = feature + "prev" + str(idx_back) + "isdigit" + "=" + str(int(str.isdigit(words[j])))+" "
-                #feature = feature + "prev" + str(idx_back) + "isalpha" + "=" + str(int(str.isalpha(words[j])))+" "
+                feature = feature + "prev" + str(idx_back) + "word" + "=" + words[j] + " " + "prev" + str(
+                    idx_back) + "pos" + "=" + poss[j] + " "
+                feature = feature + "prev" + str(idx_back) + "isUpper" + "=" + str(int(str.isupper(words[j]))) + " "
+                feature = feature + "prev" + str(idx_back) + "istitle" + "=" + str(int(str.istitle(words[j]))) + " "
+                feature = feature + "prev" + str(idx_back) + "islower" + "=" + str(int(str.islower(words[j]))) + " "
+                feature = feature + "prev" + str(idx_back) + "isdigit" + "=" + str(int(str.isdigit(words[j]))) + " "
+                # feature = feature + "prev" + str(idx_back) + "isalpha" + "=" + str(int(str.isalpha(words[j])))+" "
 
         idx_back = idx_back - 1
-    
+
     feature = feature + "currentword=" + current_word + " " + "currentpos=" + current_pos + " "
-    feature = feature + "forcesmall=" + current_word.lower() + " "    
-    
-    if boolean_features:    
+    feature = feature + "forcesmall=" + current_word.lower() + " "
+
+    if boolean_features:
         feature = feature + "isalpha=" + str(int(str.isalpha(current_word))) + " "
         feature = feature + "isdigit=" + str(int(str.isdigit(current_word))) + " "
         feature = feature + "islower=" + str(int(str.islower(current_word))) + " "
@@ -81,11 +84,13 @@ for i in range(0,len(words)):
         feature = feature + "isupper=" + str(int(str.isupper(current_word))) + " "
 
     idx_forward = 1
-    for j in range(i+1,i+1+forward_window_size):
-        if j >= len(words) :
-            feature = feature + "next" + str(idx_forward) + "word" + "=" + default_word + " " + "next" + str(idx_forward) + "pos" + "=" + default_pos+" "
+    for j in range(i + 1, i + 1 + forward_window_size):
+        if j >= len(words):
+            feature = feature + "next" + str(idx_forward) + "word" + "=" + default_word + " " + "next" + str(
+                idx_forward) + "pos" + "=" + default_pos + " "
         else:
-            feature = feature + "next" + str(idx_forward) + "word" + "=" + words[j] + " " + "next" + str(idx_forward) + "pos" + "=" + poss[j] + " "
+            feature = feature + "next" + str(idx_forward) + "word" + "=" + words[j] + " " + "next" + str(
+                idx_forward) + "pos" + "=" + poss[j] + " "
         idx_forward = idx_forward + 1
 
     feature = feature + tags[i] + "\n"
